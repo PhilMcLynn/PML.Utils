f5c0023b11fe6827585e0e5a23e0969ef42f7eb6
PhilMcLynn <62393187+PhilMcLynn@users.noreply.github.com>
2020-06-15 10:53:12 +0100

    Init
    Init

---
+++ PML.PrimeNumber/PML.PrimeNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PML.PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
